b14364e661fbba2d1069a906bf4edb4b4c100c98
t0m <bobtfish@bobtfish.net>
2009-04-23 00:44:55 +0100

    Thats action registration sorted out. Now I just need to introspect all the models, and generate shim controllers, piece of piss :)

--- lib/DynamicAppDemo/ControllerBase.pm
+++ lib/DynamicAppDemo/ControllerBase.pm
@@ -1,9 +1,33 @@
 package DynamicAppDemo::ControllerBase;
 use Moose;
+use Moose::Util qw/find_meta/;
 use namespace::clean -except => 'meta';
 
 # Should not need attributes here, but what the hell..
 BEGIN { extends 'Catalyst::Controller' }
+
+around get_action_methods => sub {
+    my $orig = shift;
+    my $self = shift;
+
+    my $meta = find_meta($self);
+    
+    # FIXME - fugly, and nasty
+    return (
+        (   map { 
+                my $m = $meta->get_method($_);
+                # EPIC CHEAT to just smash the attribute definition :)
+                $m->meta->get_attribute('attributes')->set_value($m, ['Local']);
+                $m;
+            }
+            grep { ! /^(_|new|meta)/ }
+            $meta->get_method_list
+        ),
+        (
+            $self->$orig(@_)
+        )
+    ); 
+};
 
 __PACKAGE__->meta->make_immutable;
 
--- t/01app.t
+++ t/01app.t
@@ -1,7 +1,12 @@
 use strict;
 use warnings;
-use Test::More tests => 2;
+use Test::More tests => 3;
 
 BEGIN { use_ok 'Catalyst::Test', 'DynamicAppDemo' }
 
-ok( request('/')->is_success, 'Request should succeed' );
+{
+    my $res = request('/fakeexample/register_me');
+    ok( $res->is_success, 'should succeed' );
+    is( $res->header('X-Foo'), 'bar', 'is calling correct code' );
+}
+
