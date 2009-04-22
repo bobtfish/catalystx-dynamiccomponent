fa2fa19949b443208aabc6966454ceed8bc2b11f
t0m <bobtfish@bobtfish.net>
2009-04-22 22:36:00 +0100

    Strip code down to the bare minimum to start from

--- lib/DynamicAppDemo.pm
+++ lib/DynamicAppDemo.pm
@@ -1,64 +1,19 @@
 package DynamicAppDemo;
+use Moose;
+use Catalyst::Runtime '5.80002';
 
-use strict;
-use warnings;
+use Catalyst qw/
+    -Debug
+    ConfigLoader
+/;
 
-use Catalyst::Runtime '5.70';
+extends 'Catalyst';
 
-# Set flags and add plugins for the application
-#
-#         -Debug: activates the debug mode for very useful log messages
-#   ConfigLoader: will load the configuration from a Config::General file in the
-#                 application's home directory
-# Static::Simple: will serve static files from the application's root
-#                 directory
-
-use parent qw/Catalyst/;
-use Catalyst qw/-Debug
-                ConfigLoader
-                Static::Simple/;
 our $VERSION = '0.01';
-
-# Configure the application.
-#
-# Note that settings in dynamicappdemo.conf (or other external
-# configuration file that you set up manually) take precedence
-# over this when using ConfigLoader. Thus configuration
-# details given here can function as a default configuration,
-# with an external configuration file acting as an override for
-# local deployment.
 
 __PACKAGE__->config( name => 'DynamicAppDemo' );
 
-# Start the application
 __PACKAGE__->setup();
 
+__PACKAGE__->meta->make_immutable;
 
-=head1 NAME
-
-DynamicAppDemo - Catalyst based application
-
-=head1 SYNOPSIS
-
-    script/dynamicappdemo_server.pl
-
-=head1 DESCRIPTION
-
-[enter your description here]
-
-=head1 SEE ALSO
-
-L<DynamicAppDemo::Controller::Root>, L<Catalyst>
-
-=head1 AUTHOR
-
-Tomas Doran,,,,
-
-=head1 LICENSE
-
-This library is free software, you can redistribute it and/or modify
-it under the same terms as Perl itself.
-
-=cut
-
-1;
