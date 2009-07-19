c7047fede97551c9a0e3a75db5efea1f6aad996f
t0m <bobtfish@bobtfish.net>
2009-07-19 04:52:19 +0100

    Remove blanking COMPONENT method

--- t/lib/TestComplexApp.pm
+++ t/lib/TestComplexApp.pm
@@ -12,16 +12,11 @@ extends 'Catalyst';
 
 our $VERSION = '0.01';
 
-__PACKAGE__->config( 
+__PACKAGE__->config(
     name => 'TestComplexApp',
     'CatalystX::DynamicComponent::ModelsFromConfig' => {
         include => 'PaymentProvider::',
         roles => 'TestComplexApp::ProfileModelLoader',
-        methods => {
-            COMPONENT => undef, # Remove custom COMPONENT method as
-                                # we're doing our own thing with
-                                # InstancePerContext
-        },
     },
     'Model::PaymentProvider::Datacash' => {
         class => 'PaymentProvider::Datacash',
@@ -37,7 +32,7 @@ __PACKAGE__->config(
 # Ensure to load the model class early and so fail early..
 sub _setup_dynamic_model_config {
     my ($app, $model_name, $config) = @_;
-    
+
     die("Config for $model_name does not have a ->{class} key, fatal!\n")
         unless $config->{class};
 
